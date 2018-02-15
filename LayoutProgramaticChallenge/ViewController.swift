8ad0658ae18619520b98e835c876a41a4eeb3716
Eduardo Vital Alencar Cunha <vitaldu@gmail.com>
2018-02-15 15:11:26 -0200

    Initial commit

---
+++ LayoutProgramaticChallenge/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  LayoutProgramaticChallenge
+//
+//  Created by Eduardo Vital Alencar Cunha on 28/06/17.
+//  Copyright © 2017 BEPiD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let redRect = UIView()
+        let greyRect = UIView()
+        let blueRect = UIView()
+        let yellowRect = UIView()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
